ae1af1c08b768c28f97d2e29ee3eed0c9ce0ac3a
Alexander Marcondes Junior <alexmarcjr21@gmail.com>
2024-07-01 18:21:15 +0100

    feat: widget api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { Widget, addResponseMessage, addUserMessage } from "react-chat-widget";
+import "react-chat-widget/lib/styles.css";
+import { Container } from "@mui/material";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [widgetOpened, setWidgetOpened] = useState<boolean>(false);
+
+  useEffect(() => {
+    addResponseMessage("Welcome to this awesome chat!");
+  }, []);
+
+  const handleNewUserMessage = async (newMessage: string) => {
+    addUserMessage(newMessage);
+
+    const bodyResponse = {
+      modelId: "modelo",
+      prompt: newMessage,
+    };
+
+    try {
+      const API_URL = import.meta.env.VITE_API_URL;
+      const response = await fetch(`${API_URL}/legal-assistant`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bodyResponse),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.text();
+      console.log("data ===> ", data);
+      setMessages((prevMessages) => [...prevMessages, data]);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    messages.forEach((message) => addResponseMessage(message));
+  }, [messages]);
+
+  const handleClickWidgetContainer = () => {
+    setWidgetOpened((prevOpened) => !prevOpened); // Alternar entre true e false
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Container
+      style={{ color: "black", cursor: "pointer" }}
+      className="App"
+      maxWidth="sm"
+      onClick={handleClickWidgetContainer}
+    >
+      <Widget
+        handleNewUserMessage={handleNewUserMessage}
+        title="My new awesome title"
+        subtitle="And my cool subtitle"
+      />
+    </Container>
+  );
+};
 
-export default App
+export default App;
